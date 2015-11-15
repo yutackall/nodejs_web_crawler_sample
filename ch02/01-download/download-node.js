4719ef7d26f1887d0c6e722431cdb8e673629407
yutackall <yutackall@gmail.com>
2015-11-15 18:47:23 +0900

    第2章01の download-node.js を追加

---
+++ ch02/01-download/download-node.js
@@ -0,0 +1,15 @@
+var url = "http://kujirahand.com/";
+var savepath = "test.html";
+
+var http = require('http');
+var fs = require('fs');
+
+var outfile = fs.createWriteStream(savepath);
+
+http.get(url, function(res){
+  res.pipe(outfile);
+  res.on('end', function(){
+    outfile.close();
+    console.log("ok");
+  });
+});

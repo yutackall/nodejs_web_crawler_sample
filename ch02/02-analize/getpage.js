b9305e9da458530180b4bc4e7193d3df66ce3c6d
yutackall <yutackall@gmail.com>
2015-11-15 19:01:25 +0900

    第2章02の getpage.js を作成

---
+++ ch02/02-analize/getpage.js
@@ -0,0 +1,8 @@
+var client = require('cheerio-httpcli');
+var url = "http://www.aozora.gr.jp/index_pages/person81.html";
+var param = {};
+client.fetch(url, param, function(err, $, res) {
+  if (err) { console.log("Error:", err); return; }
+  var body =$.html();
+  console.log(body);
+});
